2a25bc9e88173fa415a9db4863c757d3f4e03c6c
gallacherc <chloe.gallacher@cgi.com>
2023-07-27 11:15:52 +0100

    NAL-643: Refactoring gitlab config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  presets: [
-    '@babel/preset-env',
-    ['@babel/preset-react', {runtime: 'automatic'}],
-  ],
+  presets: ['@babel/preset-env', ['@babel/preset-react', { runtime: 'automatic' }]],
+  plugins: ['transform-class-properties', 'istanbul'],
 };
