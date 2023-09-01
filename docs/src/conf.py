549348ba90ecd40554b3042ce692936a69dabc47
t.swain <trevor.swain@cgi.com>
2023-09-01 09:53:35 +0100

    NAL-678 : Added testId to ToggleButtons

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2023, SKAO'
 author = 'SKAO, (Trevor A Swain)'
 
 # The short X.Y version
-version = '0.1.55'
+version = '0.1.56'
 # The full version, including alpha/beta/rc tags
-release = '0.1.55'
+release = '0.1.56'
 
 # -- General configuration ---------------------------------------------------
 
--- src/components/version/index.ts
+++ src/components/version/index.ts
@@ -1,1 +1,1 @@
-export const GUI_COMPONENTS_VERSION = '0.1.55';
+export const GUI_COMPONENTS_VERSION = '0.1.56';
