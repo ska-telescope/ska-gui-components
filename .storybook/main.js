aa856b6a9c149317a63ac64b5f90cc19f13d9fc4
t.swain <trevor.swain@cgi.com>
2023-11-08 13:15:50 +0000

    STAR-79 : Update of Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,8 +5,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-mdx-gfm"
+    "@storybook/preset-create-react-app"
   ],
 
   framework: {
--- src/components/version/index.ts
+++ src/components/version/index.ts
@@ -1,1 +1,1 @@
-export const GUI_COMPONENTS_VERSION = '0.1.64';
+export const GUI_COMPONENTS_VERSION = '0.1.65';
