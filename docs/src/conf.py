c3c4e2d9182a2ea1dd6dca0e1251be5202de7b52
t.swain <trevor.swain@cgi.com>
2023-11-16 13:30:19 +0000

    STAR-79 : Resolved Vunerabilities

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2023, SKAO'
 author = 'SKAO, (Trevor A Swain)'
 
 # The short X.Y version
-version = '0.1.66'
+version = '0.1.67'
 # The full version, including alpha/beta/rc tags
-release = '0.1.66'
+release = '0.1.67'
 
 # -- General configuration ---------------------------------------------------
 
