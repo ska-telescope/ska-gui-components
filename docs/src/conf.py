8302e2b0c64d005e27eae9b53463a31ec7bdd6e4
t.swain <trevor.swain@cgi.com>
2024-02-23 16:22:31 +0000

    STAR-181 : Fixed issue in conf.py

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,15 +6,9 @@ copyright = '2023, SKAO'
 author = 'SKAO, (Trevor A Swain)'
 
 # The short X.Y version
-<<<<<<< HEAD
 version = '0.1.95'
 # The full version, including alpha/beta/rc tags
 release = '0.1.95'
-=======
-version = '0.1.94'
-# The full version, including alpha/beta/rc tags
-release = '0.1.94'
->>>>>>> main
 
 # -- General configuration ---------------------------------------------------
 
