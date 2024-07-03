67d4c323de0e2130133fa2e328687e73da92e1c8
Trevor Swain <trevor.swain@cgi.com>
2024-07-03 13:27:37 +0100

    STAR-345 : Updated more exported components

--- types/components/Button/Button.d.ts
+++ types/components/Button/Button.d.ts
@@ -1,32 +1,44 @@
 export declare enum ButtonColorTypes {
-    Error = "error",
-    Inherit = "inherit",
-    Secondary = "secondary",
-    Success = "success",
-    Warning = "warning"
+  Error = 'error',
+  Inherit = 'inherit',
+  Secondary = 'secondary',
+  Success = 'success',
+  Warning = 'warning',
 }
 export declare enum ButtonSizeTypes {
-    Small = "small",
-    Medium = "medium",
-    Large = "large"
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large',
 }
 export declare enum ButtonVariantTypes {
-    Contained = "contained",
-    Outlined = "outlined",
-    Text = "text"
+  Contained = 'contained',
+  Outlined = 'outlined',
+  Text = 'text',
 }
 export interface ButtonProps {
-    ariaDescription?: string;
-    color?: ButtonColorTypes;
-    component?: string;
-    disabled?: boolean;
-    icon?: JSX.Element;
-    label: string;
-    onClick?: Function;
-    size?: ButtonSizeTypes;
-    testId: string;
-    toolTip?: string;
-    variant?: ButtonVariantTypes;
+  ariaDescription?: string;
+  color?: ButtonColorTypes;
+  component?: string;
+  disabled?: boolean;
+  icon?: JSX.Element;
+  label: string;
+  onClick?: Function;
+  size?: ButtonSizeTypes;
+  testId: string;
+  toolTip?: string;
+  variant?: ButtonVariantTypes;
 }
-export declare function OurButton({ ariaDescription, color, component, disabled, icon, label, onClick, size, testId, toolTip, variant, }: ButtonProps): JSX.Element;
+export declare function OurButton({
+  ariaDescription,
+  color,
+  component,
+  disabled,
+  icon,
+  label,
+  onClick,
+  size,
+  testId,
+  toolTip,
+  variant,
+}: ButtonProps): JSX.Element;
 export default OurButton;
--- types/components/StatusIcon/StatusIcon.d.ts
+++ types/components/StatusIcon/StatusIcon.d.ts
@@ -1,13 +1,22 @@
 import React from 'react';
 interface StatusProps {
-    ariaDescription?: string;
-    ariaTitle?: string;
-    children?: React.ReactElement | string;
-    icon?: boolean;
-    level?: number;
-    size?: number;
-    testId: string;
-    text?: string;
+  ariaDescription?: string;
+  ariaTitle?: string;
+  children?: React.ReactElement | string;
+  icon?: boolean;
+  level?: number;
+  size?: number;
+  testId: string;
+  text?: string;
 }
-export declare function StatusIcon({ ariaDescription, ariaTitle, children, icon, level, size, testId, text, }: StatusProps): React.JSX.Element;
+export declare function StatusIcon({
+  ariaDescription,
+  ariaTitle,
+  children,
+  icon,
+  level,
+  size,
+  testId,
+  text,
+}: StatusProps): React.JSX.Element;
 export default StatusIcon;
