18d1656c715f90e08fc30f2660504074f74a3ccc
Trevor Swain <trevor.swain@cgi.com>
2024-07-16 18:34:02 +0100

    STAR-345 : Updated to 2.0.12

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2023, SKAO'
 author = 'SKAO, (Trevor A Swain)'
 
 # The short X.Y version
-version = '2.0.11'
+version = '2.0.12'
 # The full version, including alpha/beta/rc tags
-release = '2.0.11'
+release = '2.0.12'
 
 # -- General configuration ---------------------------------------------------
 
