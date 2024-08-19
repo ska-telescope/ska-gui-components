a39831cf641f608d22400c9420a5f3d306155c43
Trevor Swain <trevor.swain@cgi.com>
2024-08-19 10:26:55 +0100

    STAR-345 : Added Ability to use the SKAO Symbol to the Header

--- types/components/SearchEntry/SearchEntry.d.ts
+++ types/components/SearchEntry/SearchEntry.d.ts
@@ -1,20 +1,31 @@
 export declare enum STATE {
-    OK = "ok",
-    ERROR = "error",
-    NONE = "none",
-    UNKNOWN = "unknown"
+  OK = 'ok',
+  ERROR = 'error',
+  NONE = 'none',
+  UNKNOWN = 'unknown',
 }
 export interface SearchEntryProps {
-    label: string;
-    testId: string;
-    value: string | number;
-    ariaDescription?: string;
-    ariaTitle?: string;
-    disabled?: boolean;
-    errorText?: string;
-    helperText?: string;
-    onFocus?: Function;
-    setValue?: Function;
+  label: string;
+  testId: string;
+  value: string | number;
+  ariaDescription?: string;
+  ariaTitle?: string;
+  disabled?: boolean;
+  errorText?: string;
+  helperText?: string;
+  onFocus?: Function;
+  setValue?: Function;
 }
-export declare function SearchEntry({ ariaDescription, ariaTitle, disabled, errorText, helperText, label, onFocus, setValue, testId, value, }: SearchEntryProps): JSX.Element;
+export declare function SearchEntry({
+  ariaDescription,
+  ariaTitle,
+  disabled,
+  errorText,
+  helperText,
+  label,
+  onFocus,
+  setValue,
+  testId,
+  value,
+}: SearchEntryProps): JSX.Element;
 export default SearchEntry;
