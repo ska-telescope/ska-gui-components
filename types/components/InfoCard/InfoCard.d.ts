cd6538b8c71c7566c48fab28e6dcb9befbb12c2d
Trevor Swain <trevor.swain@cgi.com>
2024-10-21 13:10:01 +0100

    STAR-748 : Linting

--- types/components/InfoCard/InfoCard.d.ts
+++ types/components/InfoCard/InfoCard.d.ts
@@ -1,27 +1,40 @@
 import React from 'react';
 export declare enum InfoCardColorTypes {
-    Error = "error",
-    Info = "info",
-    Success = "success",
-    Warning = "warning"
+  Error = 'error',
+  Info = 'info',
+  Success = 'success',
+  Warning = 'warning',
 }
 export declare enum InfoCardVariantTypes {
-    Filled = "filled",
-    Outlined = "outlined"
+  Filled = 'filled',
+  Outlined = 'outlined',
 }
 export interface InfoCardProps {
-    ariaDescription?: string;
-    ariaTitle?: string;
-    clickFunction?: Function;
-    color?: InfoCardColorTypes;
-    fontSize?: number;
-    maxHeight?: string;
-    minHeight?: string;
-    message: string;
-    showStatus?: boolean;
-    showStatusIcon?: boolean;
-    testId: string;
-    variant?: InfoCardVariantTypes;
+  ariaDescription?: string;
+  ariaTitle?: string;
+  clickFunction?: Function;
+  color?: InfoCardColorTypes;
+  fontSize?: number;
+  maxHeight?: string;
+  minHeight?: string;
+  message: string;
+  showStatus?: boolean;
+  showStatusIcon?: boolean;
+  testId: string;
+  variant?: InfoCardVariantTypes;
 }
-export declare function InfoCard({ ariaDescription, ariaTitle, clickFunction, color, fontSize, maxHeight, minHeight, message, showStatus, showStatusIcon, testId, variant, }: InfoCardProps): React.JSX.Element;
+export declare function InfoCard({
+  ariaDescription,
+  ariaTitle,
+  clickFunction,
+  color,
+  fontSize,
+  maxHeight,
+  minHeight,
+  message,
+  showStatus,
+  showStatusIcon,
+  testId,
+  variant,
+}: InfoCardProps): React.JSX.Element;
 export default InfoCard;
--- types/components/NumberEntry/NumberEntry.d.ts
+++ types/components/NumberEntry/NumberEntry.d.ts
@@ -1,27 +1,48 @@
 import React from 'react';
 import { LABEL_POSITION } from '../EntryField/EntryField';
 export interface NumberEntryProps {
-    label: string;
-    value: number | string;
-    ariaDescription?: string;
-    ariaTitle?: string;
-    disabled?: boolean;
-    errorText?: string;
-    height?: number;
-    helperText?: string;
-    inputRef?: React.MutableRefObject<any>;
-    labelBold?: boolean;
-    labelPosition?: LABEL_POSITION;
-    labelWidth?: number;
-    password?: boolean;
-    name?: string;
-    onBlur?: Function;
-    onFocus?: Function;
-    prefix?: JSX.Element | string;
-    required?: boolean | string;
-    setValue?: Function;
-    suffix?: JSX.Element | string;
-    testId: string;
+  label: string;
+  value: number | string;
+  ariaDescription?: string;
+  ariaTitle?: string;
+  disabled?: boolean;
+  errorText?: string;
+  height?: number;
+  helperText?: string;
+  inputRef?: React.MutableRefObject<any>;
+  labelBold?: boolean;
+  labelPosition?: LABEL_POSITION;
+  labelWidth?: number;
+  password?: boolean;
+  name?: string;
+  onBlur?: Function;
+  onFocus?: Function;
+  prefix?: JSX.Element | string;
+  required?: boolean | string;
+  setValue?: Function;
+  suffix?: JSX.Element | string;
+  testId: string;
 }
-export declare function NumberEntry({ ariaDescription, ariaTitle, disabled, errorText, height, helperText, inputRef, label, labelBold, labelPosition, labelWidth, name, onBlur, onFocus, prefix, required, setValue, suffix, testId, value, }: NumberEntryProps): JSX.Element;
+export declare function NumberEntry({
+  ariaDescription,
+  ariaTitle,
+  disabled,
+  errorText,
+  height,
+  helperText,
+  inputRef,
+  label,
+  labelBold,
+  labelPosition,
+  labelWidth,
+  name,
+  onBlur,
+  onFocus,
+  prefix,
+  required,
+  setValue,
+  suffix,
+  testId,
+  value,
+}: NumberEntryProps): JSX.Element;
 export default NumberEntry;
