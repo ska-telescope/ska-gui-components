e0874af816a7a56889578cc1fe23f8523d0daa23
Trevor Swain <trevor.swain@cgi.com>
2024-11-21 15:57:03 +0000

    STAR-873 : Maintenance and addition of toolTips to a number of components

--- types/components/SearchEntry/SearchEntry.d.ts
+++ types/components/SearchEntry/SearchEntry.d.ts
@@ -1,21 +1,33 @@
 export declare enum STATE {
-    OK = "ok",
-    ERROR = "error",
-    NONE = "none",
-    UNKNOWN = "unknown"
+  OK = 'ok',
+  ERROR = 'error',
+  NONE = 'none',
+  UNKNOWN = 'unknown',
 }
 export interface SearchEntryProps {
-    label: string;
-    testId: string;
-    value: string | number;
-    ariaDescription?: string;
-    ariaTitle?: string;
-    disabled?: boolean;
-    errorText?: string;
-    helperText?: string;
-    onFocus?: Function;
-    setValue?: Function;
-    toolTip?: string;
+  label: string;
+  testId: string;
+  value: string | number;
+  ariaDescription?: string;
+  ariaTitle?: string;
+  disabled?: boolean;
+  errorText?: string;
+  helperText?: string;
+  onFocus?: Function;
+  setValue?: Function;
+  toolTip?: string;
 }
-export declare function SearchEntry({ ariaDescription, ariaTitle, disabled, errorText, helperText, label, onFocus, setValue, testId, toolTip, value, }: SearchEntryProps): JSX.Element;
+export declare function SearchEntry({
+  ariaDescription,
+  ariaTitle,
+  disabled,
+  errorText,
+  helperText,
+  label,
+  onFocus,
+  setValue,
+  testId,
+  toolTip,
+  value,
+}: SearchEntryProps): JSX.Element;
 export default SearchEntry;
