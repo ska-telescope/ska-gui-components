5d190cb0b6847a5b71107d67e85e664eca8d6b3c
Trevor Swain <trevor.swain@cgi.com>
2024-12-02 12:45:47 +0000

    STAR-873 : Authprovider

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2023, SKAO'
 author = 'SKAO, (Trevor A Swain)'
 
 # The short X.Y version
-version = '2.1.00'
+version = '2.1.01'
 # The full version, including alpha/beta/rc tags
-release = '2.1.00'
+release = '2.1.01'
 
 # -- General configuration ---------------------------------------------------
 
--- types/components/version/index.d.ts
+++ types/components/version/index.d.ts
@@ -1,1 +1,1 @@
-export declare const GUI_COMPONENTS_VERSION = '2.1.00';
+export declare const GUI_COMPONENTS_VERSION = '2.1.01';
