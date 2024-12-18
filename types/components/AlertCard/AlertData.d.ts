4fca1aa306af3d4b7068d4e399a857772a0e4b5c
Trevor Swain <trevor.swain@cgi.com>
2024-12-18 09:56:34 +0000

    STAR-923 : Updated index.html

--- types/components/AlertCard/AlertData.d.ts
+++ types/components/AlertCard/AlertData.d.ts
@@ -1,8 +1,8 @@
 import { AlertColorTypes, AlertVariantTypes } from '../Alert/Alert.js';
 export declare const ALERT_DATA: {
-  color: AlertColorTypes;
-  title: string;
-  variant: AlertVariantTypes;
-  value: number;
-  hideValue: boolean;
+    color: AlertColorTypes;
+    title: string;
+    variant: AlertVariantTypes;
+    value: number;
+    hideValue: boolean;
 }[];
--- types/components/ButtonLogin/ButtonLogin.d.ts
+++ types/components/ButtonLogin/ButtonLogin.d.ts
@@ -1,25 +1,14 @@
 import { ButtonColorTypes, ButtonSizeTypes, ButtonVariantTypes } from '../Button/Button.js';
 export interface ButtonLoginProps {
-  ariaDescription?: string;
-  color?: ButtonColorTypes;
-  component?: string;
-  disabled?: boolean;
-  label: string;
-  onError?: Function;
-  size?: ButtonSizeTypes;
-  testId?: string;
-  toolTip?: string;
-  variant?: ButtonVariantTypes;
+    ariaDescription?: string;
+    color?: ButtonColorTypes;
+    component?: string;
+    disabled?: boolean;
+    label: string;
+    onError?: Function;
+    size?: ButtonSizeTypes;
+    testId?: string;
+    toolTip?: string;
+    variant?: ButtonVariantTypes;
 }
-export declare function ButtonLogin({
-  ariaDescription,
-  color,
-  component,
-  disabled,
-  label,
-  onError,
-  size,
-  testId,
-  toolTip,
-  variant,
-}: ButtonLoginProps): JSX.Element;
+export declare function ButtonLogin({ ariaDescription, color, component, disabled, label, onError, size, testId, toolTip, variant, }: ButtonLoginProps): JSX.Element;
