c6d3ee03ca8882c14c147cdae448aacd29a8b2b4
Trevor Swain <trevor.swain@cgi.com>
2024-12-18 08:57:22 +0000

    STAR-923 : Update build process

--- types/components/HelpModal/HelpModal.d.ts
+++ types/components/HelpModal/HelpModal.d.ts
@@ -1,14 +1,20 @@
 export type Help = {
-    content: Object;
-    component: Object;
-    showHelp: Boolean;
+  content: Object;
+  component: Object;
+  showHelp: Boolean;
 };
 export interface HelpModalProps {
-    ariaDescription?: string;
-    component?: boolean;
-    help: Help;
-    helpToggle: Function;
-    toolTip?: string;
+  ariaDescription?: string;
+  component?: boolean;
+  help: Help;
+  helpToggle: Function;
+  toolTip?: string;
 }
-export declare function HelpModal({ ariaDescription, component, help, helpToggle, toolTip, }: HelpModalProps): JSX.Element;
+export declare function HelpModal({
+  ariaDescription,
+  component,
+  help,
+  helpToggle,
+  toolTip,
+}: HelpModalProps): JSX.Element;
 export default HelpModal;
--- types/components/version/index.d.ts
+++ types/components/version/index.d.ts
@@ -1,1 +1,1 @@
-export declare const GUI_COMPONENTS_VERSION = "2.1.11";
+export declare const GUI_COMPONENTS_VERSION = '2.1.11';
