cb5e18b122474a6e8c3567b53de031c18b496739
Trevor Swain <trevor.swain@cgi.com>
2025-03-17 09:36:57 +0000

    STAR-1013 : Separated TreeView imports into separate pieces

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2023, SKAO'
 author = 'SKAO, (Trevor A Swain)'
 
 # The short X.Y version
-version = '2.2.15'
+version = '2.2.16'
 # The full version, including alpha/beta/rc tags
-release = '2.2.15'
+release = '2.2.16'
 
 # -- General configuration ---------------------------------------------------
 
--- src/components/version/index.ts
+++ src/components/version/index.ts
@@ -1,1 +1,1 @@
-export const GUI_COMPONENTS_VERSION = '2.2.15';
+export const GUI_COMPONENTS_VERSION = '2.2.16';
