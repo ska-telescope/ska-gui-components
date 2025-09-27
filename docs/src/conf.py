30f11e37aa12e1fcd14a30c1ca41f8f727e52782
Trevor Swain <trevor.swain@cgi.com>
2025-09-27 16:28:26 +0100

    STAR-1696 : Color is now theme aware

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2023, SKAO'
 author = 'SKAO, (Trevor A Swain)'
 
 # The short X.Y version
-version = '2.3.33'
+version = '2.3.34'
 # The full version, including alpha/beta/rc tags
-release = '2.3.3'
+release = '2.3.34'
 
 # -- General configuration ---------------------------------------------------
 
--- src/components/version/index.ts
+++ src/components/version/index.ts
@@ -1,1 +1,1 @@
-export const GUI_COMPONENTS_VERSION = '2.3.33';
+export const GUI_COMPONENTS_VERSION = '2.3.34';
