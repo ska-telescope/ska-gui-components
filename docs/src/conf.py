f4664e73ccdabeb2abfd9fc7033e64efce2409f5
Trevor Swain <trevor.swain@cgi.com>
2025-09-29 08:53:13 +0100

    STAR-1696 : Further icon adjustments

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2023, SKAO'
 author = 'SKAO, (Trevor A Swain)'
 
 # The short X.Y version
-version = '2.3.35'
+version = '2.3.36'
 # The full version, including alpha/beta/rc tags
-release = '2.3.35'
+release = '2.3.36'
 
 # -- General configuration ---------------------------------------------------
 
--- types/components/version/index.d.ts
+++ types/components/version/index.d.ts
@@ -1,1 +1,1 @@
-export declare const GUI_COMPONENTS_VERSION = '2.3.35';
+export declare const GUI_COMPONENTS_VERSION = '2.3.36';
