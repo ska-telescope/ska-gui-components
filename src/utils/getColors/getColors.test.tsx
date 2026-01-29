2e5fc8a1bc6c4677ce74f286bc3f2f1678f5e928
Trevor Swain <trevor.swain@cgi.com>
2026-01-29 14:18:28 +0000

    STAR-1825 : Update to displays

--- src/utils/getColors/getColors.test.tsx
+++ src/utils/getColors/getColors.test.tsx
@@ -37,9 +37,9 @@ describe('getColors', () => {
       paletteIndex: 0,
     });
     expect(result).toBeDefined();
-    expect(result!.continuum.bg).toContain('rgba(237, 201, 73, 1)');
-    expect(result!.spectral.bg).toContain('rgba(118, 184, 214, 1)');
-    expect(result!.pst.bg).toContain('rgba(140, 97, 215, 1)');
+    expect(result!.continuum.bg).toContain('rgba(118, 184, 214, 1)');
+    expect(result!.spectral.bg).toContain('rgba(211, 114, 149, 1)');
+    expect(result!.pst.bg).toContain('rgba(160, 124, 94, 1)');
   });
 
   it('should return semantic colors from Original Default palette when paletteIndex=1', () => {
