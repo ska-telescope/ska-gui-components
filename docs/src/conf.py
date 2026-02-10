007e1327ac8701be40234e2100ba245933014d29
Trevor Swain <trevor.swain@cgi.com>
2026-02-10 17:41:22 +0000

    STAR-1825 : Theme update

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2023, SKAO'
 author = 'SKAO, (Trevor A Swain)'
 
 # The short X.Y version
-version = '2.8.0'
+version = '2.8.1'
 # The full version, including alpha/beta/rc tags
-release = '2.8.0'
+release = '2.8.1'
 
 # -- General configuration ---------------------------------------------------
 
--- src/components/version/index.ts
+++ src/components/version/index.ts
@@ -1,1 +1,1 @@
-export const GUI_COMPONENTS_VERSION = '2.8.0';
+export const GUI_COMPONENTS_VERSION = '2.8.1';
