a1c396bccb91a35b9f2c8a8088716aa267477116
Muhammad <isa.muhammad@IT4000355.local>
2026-07-08 11:40:53 +0100

    REL-3163: bump release to 2.8.39

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2023, SKAO'
 author = 'SKAO, (Trevor A Swain)'
 
 # The short X.Y version
-version = '2.8.38'
+version = '2.8.39'
 # The full version, including alpha/beta/rc tags
-release = '2.8.38'
+release = '2.8.39'
 
 # -- General configuration ---------------------------------------------------
 
